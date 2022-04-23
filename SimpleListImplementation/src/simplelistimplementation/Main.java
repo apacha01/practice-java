63e2dc907878b68cbd77f69eb3d34a8412ee0826
Agustin Pacheco <agustin.pacheco1901@gmail.com>
2022-04-23 16:42:08 -0300

    Estructura básica del código, creo la clase nodos y la lista.

---
+++ SimpleListImplementation/src/simplelistimplementation/Main.java
@@ -0,0 +1,13 @@
+
+package simplelistimplementation;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleList sl = new SimpleList();
+        
+        
+    }
+    
+}
---
+++ SimpleListImplementation/src/simplelistimplementation/SimpleListNode.java
@@ -0,0 +1,23 @@
+
+package simplelistimplementation;
+
+public class SimpleListNode {
+    private int value;
+    private SimpleListNode next;
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public SimpleListNode getNext() {
+        return next;
+    }
+
+    public void setNext(SimpleListNode next) {
+        this.next = next;
+    }
+}
